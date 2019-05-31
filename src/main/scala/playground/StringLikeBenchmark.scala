ab9614121261296572364280b4c9ca3df4b740a2
jyanJing <mails2jing@gmail.com>
2019-05-31 16:29:08 -0700

    Update and rename MutableHashMapBenchmark.scala to StringLikeBenchmark.scala

---
+++ src/main/scala/playground/StringLikeBenchmark.scala
@@ -0,0 +1,13 @@
+package playground
+
+import com.twitter.util.StdBenchAnnotations
+import org.openjdk.jmh.annotations._
+
+@State(Scope.Benchmark)
+class StringLikeBenchmark extends StdBenchAnnotations {
+  @Benchmark
+  def testScalaTimesAllocation() = {
+    for(i <- 0 to 1) {
+      "*" * 1024
+    }
+  }
